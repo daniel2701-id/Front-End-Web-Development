18ada77aea03818d8772d6c8268a5df8a498792f
daniel2701-id <andidaniel73x@gmail.com>
2024-10-23 22:07:19 +0800

    Delete lecture 16 directory

--- lecture 16/components/Footer.js
+++
@@ -1,9 +0,0 @@
-// create component footer
-class FooterPage extends React.Component {
-    render() {
-        // return JSX
-        return(
-            <p>Footer</p>
-        );
-    }
-}
--- lecture 16/components/Header.js
+++
@@ -1,9 +0,0 @@
-// create component header
-class HeaderPage extends React.Component {
-    render() {
-        // return JSX
-        return(
-            <h2>Cities</h2>
-        );
-    }
-}
